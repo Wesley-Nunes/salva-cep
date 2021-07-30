28fd495866ee4a95d61087239b5071bc30c6d437
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-07-30 16:16:40 -0300

    Create the auth context, auth provider and useAuth hook

---
+++ src/context/auth.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+const AuthProvider = (props) => {
+	const [loggedIn, setLoggedIn] = useState(false);
+
+	const login = () => setLoggedIn(true);
+	const logout = () => setLoggedIn(false);
+
+	const authContextValue = {
+		loggedIn,
+		login,
+		logout,
+	};
+
+	return <AuthContext.Provider value={authContextValue} {...props} />;
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
