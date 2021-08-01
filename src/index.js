9973f2670781e88a1bd19479f6640daa6a40d337
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-08-01 11:33:41 -0300

    Add the AuthProvider and UseProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { AuthProvider } from "./context/auth";
+import { UserProvider } from "./context/user";
+
 import App from "./App";
 
 import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
