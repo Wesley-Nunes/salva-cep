2a2f1423bc406a2c95bb95692874489e1de2563d
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-08-01 13:47:51 -0300

    Add the redirect to Result page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 // React
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from "react-router-dom";
 
 // Pages
 import Welcome from "./pages/Welcome";
 import Search from "./pages/Search";
+import Result from "./pages/Result";
 import Favorite from "./pages/Favorite";
 
 import { useAuth } from "./context/auth";
@@ -19,29 +26,32 @@ export const UnauthenticatedRoutes = () => {
 
 export const AuthenticatedRoutes = () => {
 	const { logout } = useAuth();
-	const { getName } = useUser();
-
+	const { getName, getSearchStatus } = useUser();
 	const name = getName();
+	const status = getSearchStatus();
 
 	return (
 		<Router>
-			<nav className="navWrapper">
-				<ul>
-					<li>
-						<Link to="/">Pesquisar</Link>
-					</li>
-					<li>
-						<Link to="/favorite">Favoritos</Link>
-					</li>
-					<li>
-						<h3>{name}</h3>
-					</li>
+			<>
+				<nav className="navWrapper">
+					<ul>
+						<li>
+							<Link to="/">Pesquisar</Link>
+						</li>
+						<li>
+							<Link to="/favorite">Favoritos</Link>
+						</li>
+						<li>
+							<h3>{name}</h3>
+						</li>
 
-					<li>
-						<Link onClick={logout}>Sair</Link>
-					</li>
-				</ul>
-			</nav>
+						<li>
+							<Link onClick={logout}>Sair</Link>
+						</li>
+					</ul>
+				</nav>
+				{status ? <Redirect to="/result" /> : null}
+			</>
 
 			<Switch>
 				<Route exact path="/">
@@ -49,6 +59,9 @@ export const AuthenticatedRoutes = () => {
 				</Route>
 				<Route path="/favorite">
 					<Favorite />
+				</Route>
+				<Route path="/result">
+					<Result />
 				</Route>
 			</Switch>
 		</Router>
