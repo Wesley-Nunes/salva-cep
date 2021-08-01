1be6c367e204467cf811937edd79ab3c504d836c
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-08-01 11:25:30 -0300

    Add onclick to button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,11 @@
 import "../../styles/global.css";
 
 const Button = (props) => {
-  const action = props.btn;
+  const action = props.btnname;
+  const click = props.onclick;
+
   return (
-    <button type="submit" className="mediumFontSize">
+    <button type="submit" className="mediumFontSize" onClick={click}>
       {action}
     </button>
   );
