05e9426b96734e3480dd66dcf80baeb087c21130
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-08-02 11:59:28 -0300

    Add the alert

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,20 +1,34 @@
+// Api data
 import data from "../api/data";
 
+// Components
 import Button from "../components/Button";
 import CardFull from "../components/CardFull";
 
+// Context hook
 import { useUser } from "../context/user";
+
+// Style
+import "../styles/global.css";
 
 const Result = () => {
 	const { setResult } = useUser();
 	const values = data().map((e) => e);
 
+	const handleClick = () => {
+		// Send values to localStorage
+		setResult(values);
+		alert("Cep Salvo =)");
+		// Clear the searched values
+		setResult([{}]);
+	};
+
 	return (
-		<>
+		<main className="contentCentralized">
 			<h2>RESULTADOS DA PESQUISA</h2>
-			<CardFull fields={values} />
-			<Button btnname="Favoritar" onclick={() => setResult(values)} />
-		</>
+			<CardFull fields={values[0]} />
+			<Button btnname="Favoritar" onclick={handleClick} />
+		</main>
 	);
 };
 
